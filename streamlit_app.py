9e0aa7682f217ff5010879c5a97d16eff4b6d607
Joshua-Kao2007 <kaojoshua0@gmail.com>
2025-08-05 21:40:31 +0000

    Message

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(page_title="Aria", page_icon="🎭", layout="centered")
+
+st.title("🎭 Aria")
+st.markdown("Welcome to the Aria Internal Prediction System. Choose an option below:")
+
+# Navigation buttons
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("### 🔐 Admin")
+    if st.button("Go to Admin Page"):
+        st.switch_page("pages/1_Admin.py")
+
+with col2:
+    st.markdown("### 🔮 Predict")
+    if st.button("Go to Predict Page"):
+        st.switch_page("pages/2_Predict.py")
+
+# Footer
+st.markdown("---")
+st.markdown("Created by Joshua Kao © 2025")
